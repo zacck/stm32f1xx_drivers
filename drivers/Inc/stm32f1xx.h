df56c9d34b44c6c8d050b0711fde1b312f498b9d
Zacck Osiemo <coderv63@gmail.com>
2023-11-10 06:50:18 +0200

    Document bus domains and memory boundaries

--- drivers/Inc/stm32f1xx.h
+++ drivers/Inc/stm32f1xx.h
@@ -13,5 +13,13 @@
 #define ROM_BASEADDR		0x1FFFF000U
 
 
+// Bus domains of the controllers
+#define PERIPH_BASE			0xA0000000U
+#define APB1PERIPH_BASE		0x40007800U
+#define APB2PERIPH_BASE		0x40015800U
+#define AHBPERIPH_BASE		0xA0000000U
+
+
+
 
 #endif /* INC_STM32F1XX_H_ */
