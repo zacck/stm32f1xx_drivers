055b0a3497e929dd788d4612cc50556bdac9809f
Zacck Osiemo <coderv63@gmail.com>
2024-01-09 08:25:05 +0200

    Add ARM NVIC Register addresses

--- drivers/Inc/stm32f1xx.h
+++ drivers/Inc/stm32f1xx.h
@@ -12,6 +12,30 @@
 
 #define __vo volatile
 
+
+/**************** Processor specific details *********/
+//Arm Cortex MX processor NVIC ISERx register addresses
+
+#define NVIC_ISER0			((__vo uint32_t *)0xE000E100)
+#define NVIC_ISER1			((__vo uint32_t *)0xE000E104)
+#define NVIC_ISER2			((__vo uint32_t *)0xE000E108)
+#define NVIC_ISER3			((__vo uint32_t *)0xE000E10C)
+
+//Arm Cortex MX processor NVIC ICERx register addresses
+#define NVIC_ICER0			((__vo uint32_t *)0XE000E180)
+#define NVIC_ICER1			((__vo uint32_t *)0XE000E184)
+#define NVIC_ICER2			((__vo uint32_t *)0XE000E188)
+#define NVIC_ICER3			((__vo uint32_t *)0XE000E18C)
+
+
+#define NVIC_PR_BASEADDR	((__vo uint32_t *)0xE000E400)
+#define NO_PR_BITS_IMPLEMENTED 4
+
+
+
+
+/****************** End Processor ********************/
+
 #define FLASH_BASEADDR		0x08000000U
 #define SRAM_BASEADDR		0x20000000U
 #define ROM_BASEADDR		0x1FFFF000U
