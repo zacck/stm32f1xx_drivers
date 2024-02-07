6fa26fd219d03f08f5217b5a32a5c20fa9a7e9fc
Zacck Osiemo <coderv63@gmail.com>
2024-02-07 08:49:36 +0200

    Implement clock control for SPI

--- drivers/src/stm32f103xx_spi.c
+++ drivers/src/stm32f103xx_spi.c
@@ -5,4 +5,36 @@
  *      Author: zaccko
  */
 
+#include "stm32f103xx_gpio.h"
+
+/***
+ * @fn SPI_PCLK_CTRL
+ *
+ * @brief Enables or Diables Clock for a SPI port
+ *
+ * @params[pSPIx]
+ * @params[EnorDi]
+ *
+ *
+ * @return void
+ */
+void SPI_PCLK_CTRL(SPI_RegDef_t *pSPIx, uint8_t EnorDi) {
+	if (EnorDi == ENABLE) {
+		if (pSPIx == SPI1) {
+			SPI1_PCLK_EN();
+		} else if (pSPIx == SPI2) {
+			SPI2_PCLK_EN();
+		} else if (pSPIx == SPI3) {
+			SPI3_PCLK_EN();
+		}
+	} else {
+		if (pSPIx == SPI1) {
+			SPI1_PCLK_DI();
+		} else if (pSPIx == SPI2) {
+			SPI2_PCLK_DI();
+		} else if (pSPIx == SPI3) {
+			SPI3_PCLK_DI();
+		}
+	}
+}
 
