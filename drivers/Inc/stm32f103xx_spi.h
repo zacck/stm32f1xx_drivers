77d2c845ab2fe4979ab8eac3959d2683574b756b
Zacck Osiemo <coderv63@gmail.com>
2024-02-07 08:20:50 +0200

    Define SPI API prototypes

--- drivers/Inc/stm32f103xx_spi.h
+++ drivers/Inc/stm32f103xx_spi.h
@@ -9,5 +9,47 @@
 #define INC_STM32F103XX_SPI_H_
 
 
+// COnfigurable items for SPIx
+typedef struct
+{
+	uint8_t SPI_DeviceMode;
+	uint8_t SPI_BUSConfig;
+	uint8_t SPI_SclkSpeed;
+	uint8_t SPI_DFF;
+	uint8_t SPI_CPOL;
+	uint8_t SPI_CPHA;
+	uint8_t SPI_SSM;
+
+}SPI_Config_t;
+
+
+//Handle structure to actually use
+
+typedef struct
+{
+	SPI_RegDef_t *pSPIx;
+	SPI_Config_t SPIConfig;
+}SPI_Handle_t;
+
+
+// Init and DeInit APIs
+void SPI_Init(SPI_Handle_t *pSPIHandle);
+void SPI_DeInit(SPI_RegDef_t *pSPIx);
+
+
+//Clock Set Up
+void SPI_PCLK_CTRL(SPI_RegDef_t *pSPIx, uint8_t EnorDi);
+
+// Send and Receive APIs
+void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len);
+void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len);
+
+
+//SPI Interrupt Configs
+void SPI_IRQConfig(uint8_t IRQNumber, uint8_t EnorDi);
+void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint8_t IRQPriority);
+void SPI_IRQHandling(SPI_Handle_t *pHandle);
+
+
 
 #endif /* INC_STM32F103XX_SPI_H_ */
